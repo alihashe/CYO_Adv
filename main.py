0ab99117f77336e05cdcd01cbee35b4e525e509a
alihashe <alihashe@iu.edu>
2022-06-01 23:01:00 -0400

    Player introduction finished

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 ## Author: Ali Hashem
 ## Choose your own adventure game
 import time as t
+import pygame as pg
 
+#Global variables (DEPRICIATED)
+#activeControl = True
+
+
+#Code for initial set-up
 class Charater:
     """The object used to define character stats and descriptions."""
-    def __init__(self, race, classs, description, hth, gld, atk, deff, int, fth, luck):
+    def __init__(self, race, classs, name, description, hth, gld, atk, deff, int, fth, luck):
         self.race = race
         self.classs = classs
+        self.name = name
         self.description = description
         self.hth = hth
         self.gld = gld
@@ -14,7 +21,7 @@ class Charater:
         self.deff = deff
         self.int = int
         self.fth = fth
-        self.luck = luck 
+        self.luck = luck
 
     def getRace(self):
         """Returns the race of the character.\n Note: For now, only human characters."""
@@ -23,6 +30,14 @@ class Charater:
     def setRace(self, raceInput="human"):
         """Sets the race of the character.\n Note: For now, only human characters."""
         self.race = raceInput
+
+    def getName(self):
+        """Returns the name of the character."""
+        return self.name
+
+    def setName(self, nameInput="chara"):
+        """Sets the name of the character."""
+        self.name = nameInput
 
     def getHth(self):
         """Returns the amount of health a character currently has."""
@@ -140,47 +155,104 @@ class Charater:
         """Returns the character class."""
         return self.classs
 
-    def setClasss(self, classsInput="warrior"):
+    def setClasss(self, classsInput=""):
         """Sets the character starting class."""
-        classsInput = self.classs
+        self.classs = classsInput
         self.setHth(24)
+        self.setGld(5)
         if classsInput == "warrior":
-            self.description = "The most basic class.\n Use your superior strength to muscle through most problems.\n However, be wary of more intelligent foes and obstacles."
+            self.description = "The most basic class.\nUse your superior strength to muscle through most problems.\nHowever, be wary of more intelligent foes and obstacles."
             self.setAtk(10)
             self.setDeff(9)
             self.setInt(3)
             self.setFth(4)
             self.setLuck(4)
-            self.addGld(20)
+            self.addGld(15)
         if classsInput == "mage":
-            self.description = ""
+            self.description = "The intellectual class.\nUtilize your quick wits and affinty for magical arts\nto problem solve your way through the world."
             self.setAtk(4)
             self.setDeff(6)
             self.setInt(10)
             self.setFth(5)
             self.setLuck(5)
-            self.addGld(20)
+            self.addGld(15)
         if classsInput == "thief":
-            self.description = ""
+            self.description = "The most balanced class.\nWhile your well-roundedness minimizes potential weaknesses,\nyou lack the exceptionality in any particular attribute."
             self.setAtk(8)
             self.setDeff(7)
-            self.setInt(3)
-            self.setFth(2)
-            self.setLuck(9)
+            self.setInt(6)
+            self.setFth(1)
+            self.setLuck(8)
             self.addGld(25)
         if classsInput == "apostle":
-            self.description = "The strangest of the classes.\n You may not be the strongest, smartest, or luckiest of the classes,\n but unique events may only be available to you."
+            self.description = "The strangest of the classes.\nYou may not be the strongest, smartest, or luckiest of the classes,\nbut unique events may only be available to you."
             self.setAtk(4)
             self.setDeff(4)
             self.setInt(4)
             self.setFth(13)
             self.setLuck(5)
-            self.addGld(15)
-        if classsInput == "peasant":
-            self.description = "The hardest class.\n Fumble through the world with nothing\n but your intuition and sheer dumb luck."
-            self.setAtk(2)
-            self.setDeff(1)
-            self.setInt(1)
-            self.setFth(2)
-            self.setLuck(24)
             self.addGld(5)
+        if classsInput == "peasant":
+            self.description = "The hardest class.\nFumble through the world with nothing\nbut your intuition and sheer dumb luck."
+            self.setAtk(1)
+            self.setDeff(1)
+            self.setInt(3)
+            self.setFth(3)
+            self.setLuck(22)
+            self.addGld(0)
+
+
+### BEGINNING OF CHOICE-BASED CODE
+
+
+#Player Introduction
+while True:
+    playerAnswer = input("Would you like to begin your adventure? (Yes/No)\n").lower().strip()
+    if playerAnswer == "yes":
+        break
+    elif playerAnswer == "no":
+        print("Hope to see you soon!")
+        t.sleep(2)
+        exit()
+    else:
+        print("Please try again.")
+while True:
+    playerName = input("Please enter the name of your character: ").capitalize()
+    playerAnswer = input("Are you sure you want to name your character {}? (Yes/No)\n".format(playerName)).lower().strip()
+    if playerAnswer == "yes":
+        player = Charater("human", "", "", "", 0, 0, 0, 0, 0, 0, 0)
+        player.setName(playerName)
+        break
+    if playerAnswer == "no":
+        pass
+    else:
+        print("Please try again.")
+while True:
+    playerClassAnswer = input("\nChoose a starting class for your character.\n| Warrior | Mage | Thief | Apostle | Peasant |\n").lower().strip()
+    if playerClassAnswer == "warrior" or playerClassAnswer == "mage" or playerClassAnswer == "thief" or playerClassAnswer == "apostle" or playerClassAnswer == "peasant":
+        player.setClasss(playerClassAnswer.lower().strip())
+        # print(player.getClasss()) # for testing
+        print("\n")
+        print("{} CLASS:".format(playerClassAnswer).upper().strip())
+        print(player.description)
+        print("\n")
+        print("STATS:")
+        print("Attack: " + str(player.getAtk()))
+        print("Defense: " + str(player.getDeff()))
+        print("Intelligence: " + str(player.getInt()))
+        print("Faith: " + str(player.getFth()))
+        print("Luck: " + str(player.getLuck()))
+        print("\n")
+        playerAnswer = input("Are you sure you want to continue with the {} class? (Yes/No)\n".format(playerClassAnswer.lower().strip())).lower().strip()
+        if playerAnswer == "yes":
+            break
+        elif playerAnswer == "no":
+            pass #Reask the question
+        else:
+            print("Please try again.")
+    else:
+        print("INVALID CLASS. Please try again.")
+
+
+#Story Introduction
+print("{} started the story.".format(playerName))
