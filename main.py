0e16021bd83b28bfd98ee788cd28f5d93451dd46
alihashe <alihashe@iu.edu>
2022-06-12 01:13:26 -0400

    Start of story

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 ## Author: Ali Hashem
 ## Choose your own adventure game
 import time as t
+import sys
 import pygame as pg
+import random as rand
+#import PySimpleGUI as PSG
+#import tkinter as tkt
 
-#Global variables (DEPRICIATED)
+#Global Variables (DEPRICIATED)
 #activeControl = True
 
 
-#Code for initial set-up
+#Code Initialization -------------------------------------------------------------------------------------------------------------------------------
 class Charater:
     """The object used to define character stats and descriptions."""
-    def __init__(self, race, classs, name, description, hth, gld, atk, deff, int, fth, luck):
+    def __init__(self, race, classs, name, description, mor, hth, gld, atk, deff, int, fth, luck):
         self.race = race
         self.classs = classs
         self.name = name
         self.description = description
+        self.mor = mor
         self.hth = hth
         self.gld = gld
         self.atk = atk
@@ -38,6 +43,22 @@ class Charater:
     def setName(self, nameInput="chara"):
         """Sets the name of the character."""
         self.name = nameInput
+
+    def getMoral(self):
+        """Returns the morality (determined by player actions + dialogue) of the player."""
+        return self.mor
+
+    def setMoral(self, moralInput):
+        """Sets the morality (determined by player actions + dialogue) of the player."""
+        self.mor = moralInput
+
+    def increaseMoral(self, moralAdd):
+        """Increases the morality (determined by player actions + dialogue) of the player."""
+        self.mor += moralAdd
+
+    def decreaseMoral(self, moralSub):
+        """Decreases the morality (determined by player actions + dialogue) of the player."""
+        self.mor -= moralSub
 
     def getHth(self):
         """Returns the amount of health a character currently has."""
@@ -152,7 +173,7 @@ class Charater:
         self.luck -= luckSub
 
     def getClasss(self):
-        """Returns the character class."""
+        """Returns the character class.\n(Mostly used for debug)"""
         return self.classs
 
     def setClasss(self, classsInput=""):
@@ -160,6 +181,7 @@ class Charater:
         self.classs = classsInput
         self.setHth(24)
         self.setGld(5)
+        self.setMoral(0)
         if classsInput == "warrior":
             self.description = "The most basic class.\nUse your superior strength to muscle through most problems.\nHowever, be wary of more intelligent foes and obstacles."
             self.setAtk(10)
@@ -168,6 +190,7 @@ class Charater:
             self.setFth(4)
             self.setLuck(4)
             self.addGld(15)
+            self.increaseMoral(5)
         if classsInput == "mage":
             self.description = "The intellectual class.\nUtilize your quick wits and affinty for magical arts\nto problem solve your way through the world."
             self.setAtk(4)
@@ -176,6 +199,7 @@ class Charater:
             self.setFth(5)
             self.setLuck(5)
             self.addGld(15)
+            self.increaseMoral(2)
         if classsInput == "thief":
             self.description = "The most balanced class.\nWhile your well-roundedness minimizes potential weaknesses,\nyou lack the exceptionality in any particular attribute."
             self.setAtk(8)
@@ -184,6 +208,7 @@ class Charater:
             self.setFth(1)
             self.setLuck(8)
             self.addGld(25)
+            self.decreaseMoral(4)
         if classsInput == "apostle":
             self.description = "The strangest of the classes.\nYou may not be the strongest, smartest, or luckiest of the classes,\nbut unique events may only be available to you."
             self.setAtk(4)
@@ -192,6 +217,7 @@ class Charater:
             self.setFth(13)
             self.setLuck(5)
             self.addGld(5)
+            self.increaseMoral(10)
         if classsInput == "peasant":
             self.description = "The hardest class.\nFumble through the world with nothing\nbut your intuition and sheer dumb luck."
             self.setAtk(1)
@@ -200,12 +226,27 @@ class Charater:
             self.setFth(3)
             self.setLuck(22)
             self.addGld(0)
+            self.increaseMoral(8)
 
+def DelayPrint(aString):
+    """Prints a message character by character instead of all at once."""
+    for char in aString:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        t.sleep(0.035)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
 
-### BEGINNING OF CHOICE-BASED CODE
+def DelayPrintSlow(aString):
+    """Slowly prints a message character by character instead of all at once."""
+    for char in aString:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        t.sleep(0.15)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
 
-
-#Player Introduction
+#Player Introduction -------------------------------------------------------------------------------------------------------------------------------
 while True:
     playerAnswer = input("Would you like to begin your adventure? (Yes/No)\n").lower().strip()
     if playerAnswer == "yes":
@@ -220,10 +261,10 @@ while True:
     playerName = input("Please enter the name of your character: ").capitalize()
     playerAnswer = input("Are you sure you want to name your character {}? (Yes/No)\n".format(playerName)).lower().strip()
     if playerAnswer == "yes":
-        player = Charater("human", "", "", "", 0, 0, 0, 0, 0, 0, 0)
+        player = Charater("human", "", "", "", 0, 0, 0, 0, 0, 0, 0, 0)
         player.setName(playerName)
         break
-    if playerAnswer == "no":
+    elif playerAnswer == "no":
         pass
     else:
         print("Please try again.")
@@ -237,6 +278,9 @@ while True:
         print(player.description)
         print("\n")
         print("STATS:")
+        print("Health: " + str(player.getHth()))
+        print("Gold: " + str(player.getGld()))
+        print("Morality: " + str(player.getMoral()))
         print("Attack: " + str(player.getAtk()))
         print("Defense: " + str(player.getDeff()))
         print("Intelligence: " + str(player.getInt()))
@@ -254,5 +298,52 @@ while True:
         print("INVALID CLASS. Please try again.")
 
 
-#Story Introduction
-print("{} started the story.".format(playerName))
+#Statistic Window -------------------------------------------------------------------------------------------------------------------------------
+
+#PSG.theme('DarkAmber') #Color theme of the window
+#The text displayed inside the window
+#layOut = [  [PSG.Text('CURRENT STATS:')],
+#            [PSG.Text('Health: {}'.format(str(player.getHth)))],
+#            [PSG.Text('Gold: {}'.format(str(player.getGld)))],
+#            [PSG.Text('')],
+#            [PSG.Text('Attack: {}'.format(str(player.getAtk)))],
+#            [PSG.Text('Defense: {}'.format(str(player.getDeff)))],
+#            [PSG.Text('Intelligence: {}'.format(str(player.getInt)))],
+#            [PSG.Text('Faith: {}'.format(str(player.getFth)))],
+#            [PSG.Text('Luck: {}'.format(str(player.getLuck)))]]
+#Create the window
+#window = PSG.Window("{} Statitics".format(playerName), layOut)
+#Updates to the window
+#root = tkt.Tk()
+#root.geometry("300x300") # Sets window size
+#root.title("Testing") # Sets window title
+
+
+
+
+### BEGINNING OF CHOICE-BASED CODE ###
+
+
+#Story Introduction -------------------------------------------------------------------------------------------------------------------------------
+# print("{} started the story.".format(playerName)) # Used for debug
+DelayPrintSlow("...")
+t.sleep(1)
+DelayPrint("\"...h....ey..\"")
+t.sleep(3)
+DelayPrint("\"HEY! Do you plan on paying for that?\"")
+t.sleep(1)
+DelayPrint("You slowly raise your head, still spinning from the previous night of drinking.")
+t.sleep(1)
+DelayPrint("As you regain your bearings, you mutter out a response:")
+if player.classs == "warrior":
+    DelayPrint("\"Of course, good sir. How much do I owe?\"")
+elif player.classs == "mage":
+    DelayPrint("\"witty\"")
+elif player.classs == "thief":
+    DelayPrint("\"Ah well..\"")
+elif player.classs == "apostle":
+    DelayPrint("\"\"")
+elif player.classs == "peasant":
+    DelayPrint("\"ahh well..\"")
+else:
+    print("There was an error. Please check the code.")
